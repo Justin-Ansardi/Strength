ccc35bb9b120059cf770db54d37d6bedbc828b7b
Justin Ansardi <ansardi.justin@gmail.com>
2024-10-13 16:24:08 -0400

    Clean up

--- Strength_API/Controllers/HomeController.cs
+++ Strength_API/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using StrengthEFcore;
 using StrengthEFcore.DataAcessFunctions;
@@ -21,19 +20,12 @@ namespace Strength_API.Controllers
 
         public string GetAllWorkoutsForUser(int userId)
         {
-            var query =   DAF.GetUserWorkoutData(_entityContext, userId).ToList();
-            //var output = JsonConvert.SerializeObject(query.Select(x => x.ExerciseBouts.Select(y => y.SetReps)));
-            var output = JsonConvert.SerializeObject(query);
-            return output;
+            var result = DAF.GetUserWorkoutData(_entityContext, userId).ToList();
 
+            return JsonConvert.SerializeObject(result);
         }
 
        
 
     }
-
-    //public static record WorkoutDTO
-    //{
-
-    //}
 }
